81ec82204d4d20efa3fba7f65fb6b8e67322e4a2
Francisco Javier Blasco Pascual <84475547+jblasco2@users.noreply.github.com>
2022-01-20 11:44:51 +0100

    Add files via upload

---
+++ codigo/oppose.R
@@ -0,0 +1,14 @@
+
+# Establecemos directorio de trabajo
+setwd("~/Desktop/POLITICA/corpus_oppose")
+
+#Cargamos 'stylo'
+library(stylo)
+
+# Ejecutamos la función
+oppose(gui = TRUE, training.frequencies = NULL, 
+       test.frequencies = NULL,
+       training.corpus.dir = "primary_set", 
+       test.corpus.dir = "secondary_set", 
+       features = NULL, 
+       path = NULL)
